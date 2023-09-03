f997f9ed7ea47de267dd8620b47def42f5cd3319
Bl4ke36 <3du4rdo.pv@gmail.com>
2023-09-02 21:59:24 -0600

    Drag objcts

---
+++ Assets/Scripts/Drag/DraggObjct.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggObjct : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("begin drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("end drag");
+    }
+}
