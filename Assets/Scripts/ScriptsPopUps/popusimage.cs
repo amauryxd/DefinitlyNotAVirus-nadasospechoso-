0493dd728c98db1d11006dcc964c98025ca4fcf1
amauryxd <cdmx2472@amerike.edu.mx>
2023-09-09 15:57:51 -0600

    gameplaydelprimerescenario
    yasrive vamoooo

--- Assets/Scripts/ScriptsPopUps/popusimage.cs
+++ Assets/Scripts/ScriptsPopUps/popusimage.cs
@@ -8,13 +8,22 @@ public class popusimage : MonoBehaviour
     public GameObject popusimg;
     public Sprite[] splist;
     public Image fotos;
+    bool act = false;
     void Update()
     {
         if (GameObject.FindGameObjectWithTag("Popupimg"))
         {
             popusimg = GameObject.FindGameObjectWithTag("Popupimg");
-            fotos = popusimg.GetComponent<Image>();
-            fotos.sprite = splist[Random.Range(1, 4)];
+            fotos = popusimg.GetComponent<Image>();           
+            if (!act)
+            {
+                fotos.sprite = splist[Random.Range(0, 3)];
+                act = true;
+            }
+        }
+        else
+        {
+            act = false;
         }
     }
 }
