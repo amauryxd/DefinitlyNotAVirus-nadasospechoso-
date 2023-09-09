2cfa2a52b32984302627b444095e92d312a31dad
VictorJ-Ch <chavarria.martinez.victor101@gmail.com>
2023-09-09 18:43:47 -0500

    Se jodio el proyecto
    NO es cierto solo agregue la escena del game over

---
+++ Assets/Scripts/Messages/texto-final/plantillatexto_end.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "objetoTexto")]
+public class plantillatexto_end : ScriptableObject
+{
+    [TextArea(3, 15)]
+    public string textoNarrativo;
+
+    [Space(20)]
+    public string respuestaUno;
+
+
+    [Space(20)]
+    public int[] arrayReferencias = new int[4];
+
+    [Space(20)]
+    public bool quitarBotones;
+
+}
